63042922585c1306b359dfad732d03384fc16f53
jorgeb1980 <jbaugracia@gmail.com>
2016-03-28 19:44:17 +0200

    Initial commit - arguments parser library

---
+++ libargs/libargs/libargs.cpp
@@ -0,0 +1,75 @@
+﻿#include "pch.h"
+#include "libargs.h"
+
+#include <string>
+#include <list>
+#include <set>
+#include <iostream>
+
+using namespace std;
+
+// Simple macro to tell if some option is written twice
+#define CONTAINS(x, y) (std::find(x.begin(), x.end(), y) != x.end())
+
+
+// Constructor: parses the command line
+ArgumentsParser::ArgumentsParser(int argc, char* theArgv[], list<string> theOptions) {
+	for (list<string>::iterator it = theOptions.begin(); it != theOptions.end();) {
+		this->acceptableOptions.insert(*it);
+	}
+	// First, parsing options
+	bool parsingOptions = true;
+	for (int i = 1; i < argc; i++) {
+		string s(theArgv[i]);
+
+		if (s.compare("--help") == 0) {
+			if (!CONTAINS(options, s)) {
+				options.push_back(s);
+			}
+		}
+		else {
+			if (parsingOptions && s[0] == '-') {
+				string tmp = s.substr(1, string::npos);
+				for (size_t i = 0; i < tmp.length(); i++) {
+					string tmp = string(&tmp[i], 1);
+					if (!CONTAINS(options, tmp)) {
+						options.push_back(tmp);
+					}
+				}
+			}
+			if (parsingOptions && s[0] != '-') {
+				parsingOptions = false;
+			}
+			if (!parsingOptions && s[0] == '-') {
+				throw s;
+			}
+			if (!parsingOptions && s[0] != '-') {
+				// Argument
+				args.push_back(s);
+			}
+		}
+	}
+}
+
+// Prints the arguments in the console
+void ArgumentsParser::listArguments() {
+	cout << "\Options:\n";
+	for (list<string>::iterator it = this->options.begin(); it != this->options.end(); it++) {
+		string arg = *it;
+		cout << (*it).c_str() << "\n";
+	}
+	cout << "\Arguments:\n";
+	for (list<string>::iterator itF = args.begin(); itF != args.end(); itF++) {
+		cout << *itF << "\n";
+	}
+}
+
+// This method returns the list of options parsed
+list<string>& ArgumentsParser::getOptions() {
+	return options;
+}
+
+// This method returns the list of arguments parsed after the options
+list<string>& ArgumentsParser::getArgs() {
+	return args;
+}
---
+++ libargs/libargs/libargs.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include <list>
+#include <set>
+
+using namespace std;
+
+/*
+* Simple command line parser.  Will try to read the arguments combo passed by the command line.
+* It assumes it will get first some options preceded by a '-' character, either in the form
+*	of many single options (-a -b -c) or grouped behind a single '-' character (like -abc).
+* The rest of arguments are assumed to be files.
+*/
+class ArgumentsParser {
+
+private:
+	// Parsed arguments
+	list<string> options;
+
+	// Parsed files
+	list<string> args;
+
+	// Possible options
+	set<string> acceptableOptions;
+public:
+	ArgumentsParser(int theArgc, char* theArgv[], list<string> options);
+	// Simple method that reports by the command line the received arguments
+	void listArguments();
+
+	// This method returns the list of options parsed
+	list<string>& getOptions();
+
+	// This method returns the list of arguments parsed after the options
+	list<string>& getArgs();
+};
